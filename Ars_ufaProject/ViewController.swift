ff305bac293056a9548ecbde1ac47c17326352a4
Arsen R <111949265+arsen31ufa@users.noreply.github.com>
2025-06-22 23:56:52 +0500

    Initial Commit

---
+++ Ars_ufaProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Ars_ufaProject
+//
+//  Created by Арсен Рахимов on 22.06.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
